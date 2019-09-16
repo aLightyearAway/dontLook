85ef4780bf88e2eb2b666d0149a5428804951bc9
Andreas Klophmann <andreas.klophmann@gmail.com>
2019-09-16 15:55:22 +0200

    First commit

---
+++ src/components/Message.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled, {keyframes} from 'styled-components';
+import { flash } from 'react-animations';
+import styles from './Message.module.css';
+
+function Message (props){
+        const Bounce = styled.div`animation 3s ${keyframes`${flash}`} infinite`;
+        let name = props.match.params.name;
+        console.log(name);
+        return <Bounce><h1>Don't look, {name}!</h1></Bounce>
+            
+        
+}
+export default Message;
